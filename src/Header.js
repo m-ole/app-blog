ec21a580e792865179cedcfdd19eb9915342af37
m-ole <macmepok3@gmail.com>
2020-01-11 16:27:20 +0200

    v1

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Menu'
+
+const Header = () => {
+
+    return (
+        <header>
+            <div>LOGO</div>
+            <Menu />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form';
+
+const Home = () => {
+
+    return(
+        <>
+            <h1>Hello home</h1>
+            <Form />
+        </>
+    );
+}
+
+export default Home;
